97d1a247587bf03a4a0a23318b79f125daf8ffd1
Nehemias Muñoz <munoz_mar@outlook.com>
2025-10-17 00:19:05 -0300

    add: imagenes(personajes,texturas) fix: colores, tamaños y animaciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ const monsters = document.querySelectorAll(".monster");
 const timeCounter = document.getElementById("timeCounter");
 const startButton = document.getElementById("startButton");
 const scoreText = document.getElementById("score");
+const angryCatImg = "assets/images/angry_cat.png";
+const happyCatImg = "assets/images/happy_cat.png";
+const badCatImg = "assets/images/bad_cat.png";
+const characters = [badCatImg, happyCatImg];
 
 let score = 0;
 
@@ -11,11 +15,23 @@ let intervalID;
 function play() {
   monsters.forEach((monster) =>
     monster.addEventListener("click", function () {
-      incraseScore();
+      hitMonster(monster);
       monster.classList.toggle("disable");
     })
   );
   startCounter();
+}
+
+function hitMonster(monster) {
+  let actualImg = monster.firstElementChild.attributes["src"].value;
+  if (actualImg.endsWith("happy_cat.png")) {
+    changeImage(monster);
+    return;
+  }
+  if (actualImg.endsWith("bad_cat.png")) {
+    incraseScore();
+    return;
+  }
 }
 
 function startCounter() {
@@ -29,6 +45,10 @@ function startCounter() {
   intervalID = setInterval(updateTimeCounter, 1000);
 }
 
+function changeImage(monster, image = angryCatImg) {
+  monster.firstElementChild.src = image;
+}
+
 function incraseScore() {
   score++;
   scoreText.textContent = score;
@@ -38,7 +58,7 @@ function updateTimeCounter() {
   seconds--;
   timeCounter.textContent = seconds;
   if (seconds % 2 == 0) {
-    showMonster(getMonster());
+    showMonster(getMonster(), 2000);
   }
   if (seconds <= 0) {
     clearInterval(intervalID);
@@ -54,17 +74,18 @@ function getMonster() {
 }
 
 function showMonster(monster, duration = 1500) {
+  changeImage(monster, getRamdomCharacter());
   monster.classList.remove("disable");
   const animation = monster.animate(
     [
       {
-        transform: "translateY(100)",
+        transform: "translateY(100%)",
       },
       {
-        transform: "translateY(0)",
+        transform: "translateY(0px)",
       },
       {
-        transform: "translateY(100)",
+        transform: "translateY(100%)",
       },
     ],
     {
@@ -75,4 +96,8 @@ function showMonster(monster, duration = 1500) {
   animation.finished.then(() => {
     monster.classList.add("disable");
   });
+}
+
+function getRamdomCharacter() {
+  return characters[getRandomNumber(characters.length)];
 }
